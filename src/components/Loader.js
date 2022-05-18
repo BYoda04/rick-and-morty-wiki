85ffeb76c3e8f168bda3f53b4f79574d5ee21ac5
B_Yoda04 <gerardmatos1990@gmail.com>
2022-05-17 23:21:12 -0500

    beta 1.9

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -5,7 +5,7 @@ const Loader = () => {
     return (
         <div className='loader'>
             <div className='loader-img'>
-                <img src={portal} />
+                <img src={portal} alt="loading..."/>
             </div>
         </div>
     );
